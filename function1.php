80097e32c0df9c3300cde54038925b08d4615625
Olivier-01H <olivierrr002@gmail.com>
2025-09-30 12:41:27 +0200

    fuction1

---
+++ function1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    function names ($a, $b) {
+        $sum = $a+$b;
+        return $sum;
+
+    }
+    echo "The sum is equal to ".names(12,22);
+    
+    ?>
+</body>
+</html>
